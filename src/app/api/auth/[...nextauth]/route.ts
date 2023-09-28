762bf8c1b88f1e8c97d1878c7f8a88193b1e71da
Subham Ray <self.programmed@gmail.com>
2023-09-28 20:54:06 +0530

    project setup

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,33 @@
+import { db } from "@/lib/db";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import { DefaultSession, NextAuthOptions } from "next-auth";
+import NextAuth from "next-auth/next";
+import { env } from "@/lib/env.mjs"
+import GithubProvider from "next-auth/providers/github";
+
+declare module "next-auth" {
+  interface Session {
+    user: DefaultSession["user"] & {
+      id: string;
+    };
+  }
+}
+
+export const authOptions: NextAuthOptions = {
+  adapter: DrizzleAdapter(db),
+  callbacks: {
+    session: ({ session, user }) => {
+      session.user.id = user.id;
+      return session;
+    },
+  },
+  providers: [
+     GithubProvider({
+      clientId: env.GITHUB_CLIENT_ID,
+      clientSecret: env.GITHUB_CLIENT_SECRET,
+    })
+  ],
+};
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import NextAuthProvider from "@/lib/auth/Provider";
+import TrpcProvider from "@/lib/trpc/Provider";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +17,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+<NextAuthProvider>
+<TrpcProvider>{children}</TrpcProvider>
+</NextAuthProvider>
+</body>
     </html>
   )
 }
