2a4910211a418ed143e54db0c051a75d9135148b
Subham Ray <self.programmed@gmail.com>
2023-10-23 23:21:34 +0530

    moved drizzle paths to server dir paths

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,8 +2,8 @@ import type { Config } from "drizzle-kit";
 import { env } from "@/env.mjs";
 
 export default {
-  schema: "./src/lib/db/schema",
-  out: "./src/lib/db/migrations",
+  schema: "./src/server/db/schema",
+  out: "./src/server/db/migrations",
   driver: "better-sqlite",
   dbCredentials: {
     url: env.DATABASE_URL
